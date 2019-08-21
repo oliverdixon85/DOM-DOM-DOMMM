33feaea99ab7a48833794d84af527cbacab1c1a8
oliverdixon85 <dixon.oliver@gmail.com>
2019-08-21 13:12:46 -0500

    finally figured most of it out

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
 
     let btn = document.createElement("button");
     let btnText = document.createTextNode("Add Square");
@@ -6,32 +6,63 @@ document.addEventListener("DOMContentLoaded", function(){
     btn.appendChild(btnText);
     document.body.appendChild(btn);
     document.body.appendChild(containerDiv);
-    
-    
+
+
     let counter = 1;
 
-        btn.addEventListener('click', function(){
-            var square = document.createElement('div');
-            square.className = 'square';
-            square.id = counter++;
-            document.body.appendChild(square);
+    btn.addEventListener('click', function () {
+        var square = document.createElement('div');
+        square.className = 'square';
+        //assign the square's id with the value of counter
+        square.id = counter;
+        document.body.appendChild(square);
+
+        //create the text for the sqaure but don't do anything with it ...
+        //until your mouse events :)
+        let squareText = document.createTextNode(counter);
+
+
+        //remember something++ is an assignment,
+        //so it needs to be its own line
+        counter++;
+
+        square.addEventListener('mouseover', function () {
+            square.appendChild(squareText);
+        });
+
+        square.addEventListener('mouseout', function () {
+            square.removeChild(squareText)
+        });
+
+        square.addEventListener('click', function () {
+            var colors = ["red", "orange", "teal", "grey", "greenyellow"];
+            var randomColor = Math.floor(Math.random() * colors.length);
+            square.style.backgroundColor = colors[randomColor];
+        })
+
+        square.addEventListener("dblclick", function () {
+            if ((square.id) % 2 == 0) {
+                if (nextDiv = document.getElementById(square.id).nextElementSibling) {
+                    nextDiv.remove();
+                } else {
+                    alert("No boxes to remove")
+                }
+
+            } else {
+                if (prevDiv = document.getElementById(square.id).previousElementSibling) {
+                    prevDiv.remove();
+                } else {
+                    alert("No boxes to remove")
+                }
+            }
+        })
+
 
     })
-
-    square.addEventListener('mouseover', function(){
-        let squareText = document.createTextNode(square.id);
-        square.appendChild(squareText);  
-  
-    })
-
-    square.addEventListener('mouseout', function(){
-        square.removeChild(squareText)
-    });
-   
-    
-    
-
 
 })
 
+
     
+    
+
